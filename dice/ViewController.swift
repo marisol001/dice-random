f86a391e217f0ec70c524c74ecb3fe3faec79064
marisol001 <reyes-espino-marisol@hotmail.com>
2022-07-02 23:52:52 -0500

    Responsive and func button

--- dice/ViewController.swift
+++ dice/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceOneImg: UIImageView!
+    @IBOutlet weak var diceTwoImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func randomButton(_ sender: Any) {
+        print("click")
+        let dataRandom = ["dice1.png", "dice2.png", "dice3.png", "dice4.png", "dice5.png", "dice6.png"]
+        diceOneImg.image = UIImage(named: dataRandom[Int.random(in: 0...5)])
+        diceTwoImg.image = UIImage(named: dataRandom[Int.random(in: 0...5)])
+    }
 }
 
